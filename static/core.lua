c9b96771e63c7c079a2039d383c8e233f14a5b34
Collin Eggert <none@none>
2015-07-21 07:49:12 -0500

    Started the lua base. API planning.

---
+++ static/core.lua
@@ -0,0 +1,7 @@
+# System works like this:
+# Sleep for a random amount of time
+# Call the getAction REST call
+# Pass control to whatever handler is made for this
+#   Make calls for any statuses and whatnot
+# Send either a success or failure for the action
+# Loop
