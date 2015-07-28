f7aa05febe249b67030463a9fd762e58c5df1481
Collin Eggert <none@none>
2015-07-27 19:15:25 -0500

    Skeleton being made for exploring

---
+++ static/action_explore.lua
@@ -0,0 +1,23 @@
+
+-- ACTION: Explores the *surface*, logging what it bumps into, recording ceilings and holes.
+-- data:
+--   steps: INT
+
+local OK = 0
+local FOUND = 1
+-- Distance to go down before calling it a hole and getting out
+local HOLE_DEPTH = 2
+-- Distance to go up to see if it wraps over to call it a ceiling
+local CEILING_HEIGHT = 10
+
+local function detectHole()
+  
+end
+
+local function detectCeiling()
+  
+end
+
+function invoke(data)
+  
+end
