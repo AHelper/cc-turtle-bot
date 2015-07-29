b50e2bdbaeaf5aecb321c407ad7147b6fb8e7c83
Collin Eggert <ahelper2@gmail.com>
2015-07-29 00:31:35 -0500

    Adding GPLv3 license

--- static/core.lua
+++ static/core.lua
@@ -1,3 +1,20 @@
+--  cc-turtle-bot
+--  Copyright (C) 2015 Collin Eggert
+--  
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--  
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--  
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 -- System works like this:
 -- Sleep for a random amount of time
 -- Call the getAction REST call
@@ -11,7 +28,7 @@ os.loadAPI("config")
 os.loadAPI("rest")
 os.loadAPI("m")
 
-log.debug("Core")
+log.debug("cc-turtle-bot  Copyright (C) 2015 Collin Eggert\nThis program comes with ABSOLUTELY NO WARRANTY; This is free software, and you are welcome to redistribute it under certain conditions.")
 log.debug(config.id)
 
 -- Are we new?
